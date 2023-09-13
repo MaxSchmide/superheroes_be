ee104f42638065a5716357ff4d7cd4357b588374
Max Kuznetsov <kuznetsov.max.v@gmail.com>
2023-09-13 23:10:53 +0300

    edit spawn function

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -9,7 +9,10 @@ const listen = async (req: Request, res: Response) => {
   if (payload && payload.ref === "refs/heads/master") {
     console.log("Received a push event to the master branch. Deploying...");
     const deployScriptPath = "/home/ec2-user/deploy.sh";
-    const deployProcess = spawn("bash", [deployScriptPath]);
+    const workingDirectory = "/home/ec2-user";
+    const deployProcess = spawn("bash", [deployScriptPath], {
+      cwd: workingDirectory,
+    });
     deployProcess.stdout.on("data", (data) => {
       console.log(`Deployment script stdout: ${data}`);
     });
