7755251a9c4284c9cf86064866c13987560cf1a7
Max Kuznetsov <kuznetsov.max.v@gmail.com>
2023-09-13 23:12:00 +0300

    test

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ mongooseConnect();
 
 app.use(cors({ origin: "" })).use(express.json());
 
-app.get("/", (_, res) => res.status(200).send("Hello from the other side"));
+app.get("/", (_, res) => res.status(200).send("Hello from the other side!"));
 
 app.post("/webhook", webhookController.listen);
 
