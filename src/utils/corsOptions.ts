450151acabf5adde60cd05685b1d94e94e8539fd
Max Kuznetsov <kuznetsov.max.v@gmail.com>
2023-09-14 18:12:24 +0300

    edit cors options

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -5,10 +5,10 @@ const whitelist = process.env.CLIENT_ORIGINS.split(",");
 
 export const corsOptions = {
   origin: (origin: string, callback: CorsCallback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(null, false);
     }
   },
 };
