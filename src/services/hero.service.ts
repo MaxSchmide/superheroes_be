50b43c34dbcb122d89f818a06dfedf9da05dec61
Max Kuznetsov <kuznetsov.max.v@gmail.com>
2023-09-16 14:04:53 +0300

    edit get all heroes response

--- src/services/hero.service.ts
+++ src/services/hero.service.ts
@@ -10,23 +10,30 @@ const read = async ({ page, perPage }: Params) => {
   const offset = perPage * (page - 1);
   const heroes = await Hero.find({}, "nickname images")
     .skip(offset)
-    .limit(perPage);
+    .limit(perPage)
+    .exec();
 
   const allHeroesCount = await Hero.find().count();
 
+  const data = heroes.map((hero) => ({
+    _id: hero._id,
+    nickname: hero.nickname,
+    image: hero.images[0],
+  }));
+
   return {
     totalHeroes: allHeroesCount,
-    heroes,
+    heroes: data,
   };
 };
 
-const readOne = (id: string) => Hero.findById(id);
+const readOne = (id: string) => Hero.findById(id).exec();
 
 const create = (data: Partial<IHero>) => Hero.create(data);
 
 const update = (id: string, fieldsToUpdate: Partial<IHero>) =>
-  Hero.findByIdAndUpdate(id, fieldsToUpdate);
+  Hero.findByIdAndUpdate(id, fieldsToUpdate).exec();
 
-const remove = (id: string) => Hero.findByIdAndDelete(id);
+const remove = (id: string) => Hero.findByIdAndDelete(id).exec();
 
 export default { read, readOne, create, update, remove };
